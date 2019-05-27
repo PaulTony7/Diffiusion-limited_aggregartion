2f079911b865f8a473fd66de97257d56c6dbdb8a
Paul Kokot <PaulKokot98@gmail.com>
2019-05-27 12:23:12 +0200

    Colored tree

--- main.cc
+++ main.cc
@@ -7,12 +7,12 @@
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Triangle");
-    // window.setFramerateLimit(60);
+    // window.setFramerateLimit(120);
     std::srand(time(NULL));
     
     float radius = 4.f;
     std::vector<sf::Vector2u> tree;
-    sf::CircleShape leaf(radius, 4);
+    sf::CircleShape leaf(radius, 16);
     leaf.setFillColor(sf::Color::White);
 
     sf::VertexArray points(sf::Points, SCREEN_WIDTH * SCREEN_HEIGHT);
@@ -23,8 +23,10 @@ int main() {
         }
     }
 
-    points[SCREEN_WIDTH / 2 + SCREEN_WIDTH * (SCREEN_HEIGHT / 2)].color = sf::Color::Red;
-    tree.push_back(sf::Vector2u(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2));
+    sf::Vector2u initialPosition(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+    //starting seed
+    tree.push_back(initialPosition);
+    
     Particle particles[100];
     for(int i = 0; i < 100; i++) {
         particles[i].Reset(rand() % SCREEN_WIDTH / 2 + SCREEN_WIDTH / 4, rand() % SCREEN_HEIGHT / 2 + SCREEN_HEIGHT / 4);
@@ -40,27 +42,28 @@ int main() {
             else if(event.type == sf::Event::KeyPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
                 window.close();
         }
-
-        // for(int i = 0; i < 100; i++) {
-        //     points[particles[i].position.x + particles[i].position.y * SCREEN_WIDTH].color = sf::Color::White;
-        // }
-
-        for(int j = 0; j < 100; j++) {
-            particlePosition = particles[j].Move();
-            std::size_t treeSize = tree.size();
-            for(std::size_t i = 0; i < treeSize; i++) {
-                int compareX = tree[i].x - particlePosition.x;
-                int compareY = tree[i].y - particlePosition.y;
-                if(compareX * compareX <= radius * radius * 3 && compareY * compareY <= radius * radius * 3) {
-                    tree.push_back(particlePosition);
-                    particles[j].Reset(rand() % SCREEN_WIDTH, rand() % SCREEN_HEIGHT);            
+        if(tree.size() < 50000) {
+            for(int j = 0; j < 100; j++) {
+                particlePosition = particles[j].Move();
+                std::size_t treeSize = tree.size();
+                for(std::size_t i = 0; i < treeSize; i++) {
+                    int compareX = tree[i].x - particlePosition.x;
+                    int compareY = tree[i].y - particlePosition.y;
+                    if(compareX * compareX <= radius * radius * 4 && compareY * compareY <= radius * radius * 4) {
+                        tree.push_back(particlePosition);
+                        particles[j].Reset(rand() % SCREEN_WIDTH, rand() % SCREEN_HEIGHT);            
+                    }
                 }
             }
         }
+        
         window.clear();
 
         for(std::size_t i = 0; i < tree.size(); i++) {
             leaf.setPosition(sf::Vector2f(tree[i]));
+            int xDist = tree[i].x - initialPosition.x;
+            int yDist = tree[i].y - initialPosition.y;
+            leaf.setFillColor(sf::Color(0, xDist * xDist * 0.005 + 10.f, yDist * yDist * 0.005 + 10.f));
             window.draw(leaf);
         }
 
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
-a : main.cc
+program: main.cc
 	g++ Particle.cc main.cc -Wall -o a -lsfml-graphics -lsfml-system -lsfml-window
